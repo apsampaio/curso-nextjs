8632fa5121f28b218270af4bc1dc9825e1a55716
apsampaio <andre03.sampaio@gmail.com>
2021-01-22 21:49:55 -0300

    fetching data using js fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import { Title } from "../styles/pages/Home";
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>(
+    []
+  );
+
+  const apiURL = "http://localhost:3333";
+
+  useEffect(() => {
+    fetch(apiURL + "/recommended").then((response) => {
+      response.json().then((data) => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <Title>Hello World!</Title>
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(({ id, title }: IProduct) => {
+            return <li key={id}>{title}</li>;
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
