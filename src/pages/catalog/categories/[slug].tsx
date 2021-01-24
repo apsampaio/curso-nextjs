d81133e852d75674658f1677ed8b75556ea7cfaa
apsampaio <andre03.sampaio@gmail.com>
2021-01-23 22:12:30 -0300

    paginas estaticas dinâmicas

---
+++ src/pages/catalog/categories/[slug].tsx
@@ -0,0 +1,57 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface CategoryProps {
+  products: IProduct[];
+}
+
+export default function Category({ products }: CategoryProps) {
+  const { query } = useRouter();
+
+  return (
+    <div>
+      <h1>{query.slug}</h1>
+      <ul>
+        {products.map(({ id, title }: IProduct) => {
+          return <li key={id}>{title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const apiURL = "http://localhost:3333";
+  const response = await fetch(apiURL + "/categories");
+  const categories = await response.json();
+
+  const paths = categories.map((category) => {
+    return {
+      params: { slug: category.id },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps<CategoryProps> = async (ctx) => {
+  const { slug } = ctx.params;
+
+  const apiURL = "http://localhost:3333";
+  const response = await fetch(apiURL + `/products?category_id=${slug}`);
+  const products = await response.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 120,
+  };
+};
--- src/pages/mercado/frutas/[fruta].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Fruta() {
-  const { query } = useRouter();
-
-  return <h1>{query.fruta}</h1>;
-}
